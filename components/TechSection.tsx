e7f65f1c432c05426d952abc4fe920b9c0bc7e68
vlad-guzun <mernapplications@gmail.com>
2023-09-16 09:20:34 -0700

    commit

--- components/TechSection.tsx
+++ components/TechSection.tsx
@@ -28,6 +28,10 @@ const TechSection = () => {
             href_img: '/typescript2.svg',
         },
         {
+            name: 'C++',
+            href_img: '/c++.png',
+        },
+        {
             name: 'EC2/S3/Amplify/Lambda',
             href_img: '/aws3.png',
         },
